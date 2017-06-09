3f64f7177907724fa85f8615b84a19132717ce13
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-06-09 13:09:08 -0700

    Successfully added taggable

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'acts_as_votable', '~> 0.10.0'
 
+gem 'acts-as-taggable-on', '~> 4.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,11 @@ class Question < ApplicationRecord
   belongs_to :user, required: false
   has_many :answers, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :tag_taggings, class_name: 'ActsAsTaggableOn::Tagging'
+
+  acts_as_taggable
 
   acts_as_votable
-
 
 
   # belongs_to :author, foreign_key: 'user_id', class_name: 'User',  required: false
