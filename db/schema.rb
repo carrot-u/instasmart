0af99c8cdc4aae29dfea3885c92344124134678e
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-07-12 12:28:33 -0700

    Fixed the rest of answers, with Muffy's help.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628194008) do
+ActiveRecord::Schema.define(version: 20170712192305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,25 @@ ActiveRecord::Schema.define(version: 20170628194008) do
     t.text     "response"
     t.integer  "user_id"
     t.integer  "question_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "accepted",    default: false
+  end
+
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.integer  "answer_id"
+    t.text     "response"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
+  add_index "comments", ["answer_id"], name: "index_comments_on_answer_id", using: :btree
+  add_index "comments", ["question_id"], name: "index_comments_on_question_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "summary"
@@ -47,5 +61,8 @@ ActiveRecord::Schema.define(version: 20170628194008) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "comments", "answers"
+  add_foreign_key "comments", "questions"
+  add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
 end
