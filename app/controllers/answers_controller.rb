0d8a2c8a8332d15678dd29654a0186951e9ecf7c
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-07-12 19:13:27 -0700

    Added lazy execution in question and answer controllers; Added default scope to questions and answers.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,9 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :answer # , only: [:show, :edit, :update, :destroy]
+  before_action :question #, only: [:show, :edit, :update, :destroy]
 
   def new
-    @answer = Answer.new
+    # @answer = Answer.new
     @answer.question_id = @question.id
   end
 
@@ -20,12 +20,28 @@ class AnswersController < ApplicationController
       params.require(:answer).permit(:user_id, :question_id, :response)
     end
 
-    def set_answer
-      @answer = Answer.find(params[:id])
+    # def set_answer
+    #   @answer = Answer.find(params[:id])
+    # end
+
+    def answer
+      @answer ||= begin
+        answer = params[:id] ? Answer.find(params[:id]) : Answer.new
+        if answer_params && answer_params.length > 0
+          answer.update_attributes(answer_params)
+       end
+      end
     end
 
-    def set_question
-      @question = Question.find(params[:question_id])
+    # def set_question
+    #   @question = Question.find(params[:question_id])
+    # end
+
+    def question
+      @question ||= begin
+        raise "Question id must be provided" unless params[:question_id]
+        Question.find(params[:question_id])
+      end
     end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   has_many :comments
+  default_scope { order(created_at: :desc) }
 end
