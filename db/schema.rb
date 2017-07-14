e9f2512ad2066e03410092c0c61d15c756e142cb
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-07-14 08:38:11 -0700

    Updating schema before merge

--- db/schema.rb
+++ db/schema.rb
@@ -58,21 +58,6 @@ ActiveRecord::Schema.define(version: 20170714020026) do
     t.datetime "updated_at",                null: false
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id"
-    t.string   "votable_type"
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.boolean  "vote_flag"
-    t.string   "vote_scope"
-    t.integer  "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
-
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "comments", "users"
