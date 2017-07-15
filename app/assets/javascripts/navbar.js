319ce28fc7ab36e9b46b440b841cc6c617619997
Logan Murdock <logtheman@gmail.com>
2017-07-15 09:14:16 -0700

    fix navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,11 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Code for making a sticky nav when scroll below navbar position
     const nav = document.querySelector('.navbar');
-    const navOffsets = nav.getBoundingClientRect();
     let topOfNav = nav.offsetTop;
-    // let topOfNav = navOffsets.top;
-
-
 
     function fixNav() {
       if(window.scrollY >= topOfNav) {
@@ -17,36 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.classList.remove('fixed-nav');
         document.body.style.paddingTop = 0;
       }
-      console.log("scrollY", window.scrollY);
-      console.log("tops of navbar", topOfNav);
+    }
 
+    function adjustOffset() {
+      topOfNav = nav.offsetTop;
     }
 
     window.addEventListener('scroll', fixNav);
-
-
-    // const nav = document.querySelector('.navbar');
-    // const navOffsets = nav.getBoundingClientRect();
-    // const banner = document.querySelector('.banner');
-
-    // // let topOfNav = navOffsets.top;
-    // let topOfNav = nav.offsetTop;
-    // console.log("tops of navbar", topOfNav);
-    // console.log("scrollY", window.scrollY);
-    // console.log("scrollTop", nav.scrollTop);
-
-
-    // function fixNav() {
-    //   if(window.scrollY >= topOfNav){ //(topOfNav - 134)) {
-    //     document.body.style.paddingTop = nav.offsetHeight + 'px';
-    //     document.body.classList.add('fixed-nav');
-    //   } else {
-    //     document.body.classList.remove('fixed-nav');
-    //     document.body.style.paddingTop = 0;
-    //   }
-    // }
-
-    // window.addEventListener('scroll', fixNav);
+    window.addEventListener('resize', adjustOffset);
 
 
     // // Code for adding Search button and expanding nav search bar
