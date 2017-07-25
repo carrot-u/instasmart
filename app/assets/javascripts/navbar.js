f4c2f1f6d9bc732f3096e7d15e915af42eeb28c7
Logan Murdock <logtheman@gmail.com>
2017-07-25 14:14:55 -0700

    fix bootstrap javascript not loading issue.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Code for making a sticky nav when scroll below navbar position
     const nav = document.querySelector('.navbar');
-    let topOfNav = nav.offsetTop;
+    if(nav){
+      let topOfNav = nav.offsetTop;
 
-    function fixNav() {
-      if(window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
-      } else {
-        document.body.classList.remove('fixed-nav');
-        document.body.style.paddingTop = 0;
+      function fixNav() {
+        if(window.scrollY >= topOfNav) {
+          document.body.style.paddingTop = nav.offsetHeight + 'px';
+          document.body.classList.add('fixed-nav');
+        } else {
+          document.body.classList.remove('fixed-nav');
+          document.body.style.paddingTop = 0;
+        }
       }
+      window.addEventListener('scroll', fixNav);
     }
-    window.addEventListener('scroll', fixNav);
-
     // // Code for adding Search button and expanding nav search bar
     // const searchNavField = document.querySelector('.nav-search-field');
     const searchNavBar = document.querySelector('.nav-search-bar');
-    searchNavBar.addEventListener('focus', () => {
-      nav.classList.add('condense-nav');
-    });
-    searchNavBar.addEventListener('blur', () => {
-      nav.classList.remove('condense-nav');
-    });
+    if(searchNavBar){
+      searchNavBar.addEventListener('focus', () => {
+        nav.classList.add('condense-nav');
+      });
+      searchNavBar.addEventListener('blur', () => {
+        nav.classList.remove('condense-nav');
+      });
+    }
 });
