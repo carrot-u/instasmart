8cc5b148082ff1abfe271dbb24cc096a44a37c24
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-07-28 11:00:52 -0700

    Attempted sizing styles for profile-thumbnail in landing_page.scss

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], #stored in config/application.yml as part of gitignore
     {
     	:image_aspect_ratio => "square",
     	:image_size => 50,
