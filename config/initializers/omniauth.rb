8f750bea8bd8af6511ae3b1cbb2812442706368b
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-07-28 10:52:35 -0700

    Added image (unsized) to omniauth.rb, user.rb, and landing_page nav

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,8 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
+    	:image_aspect_ratio => "square",
+    	:image_size => 50,
       client_options:
       {ssl:
         {ca_file: Rails.root.join("cacert.pem").to_s}
