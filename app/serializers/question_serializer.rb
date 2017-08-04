ab1b1e771aacb0f3454fc16330eddeb9b1f6cd43
Logan Murdock <logtheman@gmail.com>
2017-08-03 20:46:34 -0700

    removed changes to serializer to avoid conflicts with Jeremy's work

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,10 +1,6 @@
 class QuestionSerializer < ActiveModel::Serializer
 	include ActionView::Helpers::DateHelper
-  attributes :id, :summary, :body, :user_id, :cached_votes_up, :cached_votes_down, :answers, :comments, :tags, :created_time_date
-
-  def created_time_date
-  	object.created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%b. %e, %Y at %l:%M %p")
-  end
+  attributes :id, :summary, :body, :user_id, :cached_votes_up, :cached_votes_down, :answers, :comments, :tags
 
   private
 	  def answers
