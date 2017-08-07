c22b26268a6de4121561bb1df96b7381761c99b5
Logan Murdock <logtheman@gmail.com>
2017-08-06 20:31:19 -0700

    add logic to ensure log a no question statement if no questions can be loaded

--- client/src/components/questions/QuestionsIndex.jsx
+++ client/src/components/questions/QuestionsIndex.jsx
@@ -18,7 +18,7 @@ class QuestionIndex extends React.Component {
 
 
   render() {
-    const listQuestions = this.props.questions.length > 0
+    const listQuestions = (this.props.questions && this.props.questions.length > 0)
       ? this.props.questions.map(question => {
           return (
             <QuestionIndexRow 
@@ -28,7 +28,7 @@ class QuestionIndex extends React.Component {
               createAnswer={this.props.actions.createAnswer} />
           );
         })
-      : null;
+      : <h2><i> No Questions Avaliable </i></h2>;
 
     return (
       <div>
