7c14cfdb442a40b97945a5c4f217e8601303a396
Logan Murdock <logtheman@gmail.com>
2017-08-07 17:34:41 -0700

    remove protypes not needed

--- client/src/components/common/TextInput.jsx
+++ client/src/components/common/TextInput.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 
 const TextInput = ({name, className, optional, labelClass, label, onChange, placeholder, value, error}) => {
   let wrapperClass = 'form-group';
