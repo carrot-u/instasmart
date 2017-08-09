bd278a350db68938b995641a3731436f4ca96242
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-09 08:12:38 -0700

    residue of user_activity attempt broke the world

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,24 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :email, :oauth_token, :oauth_expires_at, :user_activity
+  attributes :id, :name, :image, :email, :oauth_token, :oauth_expires_at
 
   has_many :questions
   has_many :answers
   has_many :comments
 
-  # def user_activity
-  # 	activity = []
-
-  # 	self.object.questions.each do |question|
-  # 		activity.push(question)
-  # 	end
-
-  # 	self.object.answers.each do |answer|
-  # 		activity.push(answer)
-  # 	end
-
-  # 	self.object.comments.each do |comment|
-  # 		activity.push(comment)
-  # 	end
-  	
-  end
 end
