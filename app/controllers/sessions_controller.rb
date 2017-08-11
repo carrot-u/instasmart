e567d7e3789db38d82486a9cf76c6f73e51aa223
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-11 10:11:25 -0700

    Client side signs in successfully; still troubleshooting send xhr request to the server

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
 
   def create
+    binding.pry
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'auth/:provider', to: 'sessions#create'
+  post 'auth/:provider', to: 'sessions#create'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
