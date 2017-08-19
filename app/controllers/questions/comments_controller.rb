8b5a700208fdfe91916f8c2b0d0e51a2ecd6fde1
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-19 10:28:31 -0700

    Removing superflous comments_controller files

--- app/controllers/questions/comments_controller.rb
+++
@@ -1,9 +0,0 @@
-class Questions::CommentsController < CommentsController
-	before_action :set_commentable
-
-  private
-
-    def set_commentable
-      @commentable = Question.find(params[:question_id])
-    end
-end
