b53ed4c8ce3ddb787e788210cc57b55364cafecd
Logan Murdock <logtheman@gmail.com>
2017-08-21 08:28:27 -0700

    add serializer changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,27 +1,41 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :email, :created_at, :user_activity
+  attributes :id, :name, :image, :email, :created_at, :user_question_activity, :user_answer_activity, :user_comment_activity, :user_votes_activity
 
-def user_activity
-  activity = []
-
-  self.object.questions.each do |question|
-    activity.push(question)
+  def user_votes_activity
+    self.object.votes
   end
 
-  self.object.answers.each do |answer|
-    activity.push(answer)
+  def user_question_activity
+    self.object.questions
   end
 
-  self.object.comments.each do |comment|
-    activity.push(comment)
+  def user_answer_activity
+    self.object.questions
   end
 
-  self.object.votes.each do |vote|
-    activity.push(vote)
+  def user_comment_activity
+    self.object.questions
   end
 
-  activity.sort_by! { |activity| activity.created_at }.reverse!
-  return activity
-end
+  def user_activity
+    self.object.questions.each do |question|
+      activity.push(question)
+    end
+
+    self.object.answers.each do |answer|
+      activity.push(answer)
+    end
+
+    self.object.comments.each do |comment|
+      activity.push(comment)
+    end
+
+    self.object.votes.each do |vote|
+      activity.push(vote)
+    end
+
+    activity.sort_by! { |activity| activity.created_at }.reverse!
+    return activity
+  end
 
 end
