acdd0842a2704d785bec2c4453f1fbcc85104787
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-23 08:47:14 -0700

    Reverting NavbarSearchfield but leaving in api calls

--- client/app/bundles/src/components/common/NavbarSearchField.jsx
+++ client/app/bundles/src/components/common/NavbarSearchField.jsx
@@ -1,43 +1,19 @@
-// Modules
 import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 
-// Project File
-import NavbarContainer from "./NavbarContainer";
-import * as questionActions from "../../actions/questionActions";
-import * as utils from "../common/utils";
-
-class NavbarSearchField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: null
-    };
-    this.loadSearchResults = this.loadSearchResults.bind(this);
-  }
-
-  onSearch(search){
-    this.props.questionActions.loadSearchResults(search);
-  }
-
-  render() {
-    return (
-      <div className="">
-        <form className="form-inline my-sm-0">
-          <input
-            className="form-control mr-sm-2 nav-search-bar"
-            type="search"
-            placeholder="Search"
-            ref="search"
-            onFocus={props.handleOnSearchFocus}
-            onBlur={props.handleOnSearchBlur}
-            onChange={props.onSearch(search)}
-          />
-        </form>
-      </div>
-    );
-  }
+const NavbarSearchField = props => {
+  return (
+    <div className="">
+      <form className="form-inline my-sm-0">
+        <input
+          className="form-control mr-sm-2 nav-search-bar"
+          type="search"
+          placeholder="Search"
+          onFocus={props.handleOnSearchFocus}
+          onBlur={props.handleOnSearchBlur}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default NavbarSearchField;
