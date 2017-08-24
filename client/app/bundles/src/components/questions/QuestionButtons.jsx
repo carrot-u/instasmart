e1c02acf837bf7e462860d058561e2c9051eaa35
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-24 09:29:18 -0700

    Added color variants as env variables to _variables.css and started uniform color styling and removed back button

--- client/app/bundles/src/components/questions/QuestionButtons.jsx
+++ client/app/bundles/src/components/questions/QuestionButtons.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const QuestionButtons = props => {
   return(
-      <div>
-        <button className={"btn btn-outline-success mr-1" + (props.pullRight ? " pull-right" : "")} onClick={(e) => props.onClickPost(e, "comment")}> 
+      <div className="action-buttons">
+        <button className={"btn btn-sm mr-1" + (props.pullRight ? " pull-right" : "")} onClick={(e) => props.onClickPost(e, "comment")}> 
           Comment 
         </button>
-        <button className={"btn btn-outline-success mr-1" + (props.pullRight ? " pull-right" : "")} onClick={(e) => props.onClickPost(e, "answer")}> 
+        <button className={"btn btn-sm mr-1" + (props.pullRight ? " pull-right" : "")} onClick={(e) => props.onClickPost(e, "answer")}> 
           Answer 
         </button>
-        {(!props.liked && <button className={"btn btn-outline-success mr-1" + (props.pullRight ? " pull-right" : "")} onClick={props.onClickLike}> Like </button>)}
+        {(!props.liked && <button className={"btn btn-sm mr-1" + (props.pullRight ? " pull-right" : "")} onClick={props.onClickLike}> Like </button>)}
         {(props.liked && <button className={"btn btn-primary mr-1" + (props.pullRight ? " pull-right" : "")}onClick={props.onClickLike}> Unlike </button>)}
 
     </div>
