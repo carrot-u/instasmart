c7c286c27eea43711d80311d68fa964c27a7e2a5
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-28 11:47:41 -0700

    profile info, coloring, and card blocks

--- client/app/bundles/src/components/users/UserProfile.jsx
+++ client/app/bundles/src/components/users/UserProfile.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 const UserProfile = props => {
   return (
-    <div className="row pt-1 pl-1 mr-0 show-question-detail">
-      <div className="col-md-4">
-        <img src={props.user.image} className="profile-image-large mx-1 my-2"/>
-      </div>
-      <div className="col-md-7 center-items">
-        <div className="col-12">
-          <h2 className=""> <i>{props.user.name} </i></h2>
-          <div className="row">
-            <small><i>Joined on {props.user.created_at}</i></small>
+    <div className="container">
+      <div className="row pt-1 pl-1 mr-0 show-question-detail">
+        <div className="card-block row">
+          <div className="col-md-4">
+            <img src={props.user.image} className="img-fluid profile-image-large"/>
+          </div>
+          <div className="col-md-8">
+            <h2 className=""> <i>{props.user.name} </i></h2>
+            <p><i>Joined on {props.user.created_at}</i></p>
           </div>
         </div>
-
       </div>
     </div>
   );
