a08960a2c64ce9ab8d33e72812248f55c9acec38
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-08-30 13:56:54 -0700

    modal layout changes part 1

--- client/app/bundles/src/components/common/NewQuestionModal.jsx
+++ client/app/bundles/src/components/common/NewQuestionModal.jsx
@@ -36,46 +36,53 @@ class NewQuestionModal extends React.Component {
           </ModalHeader>
           <ModalBody>
             <form>
-              <TextArea
-                label="Question"
-                labelClass="h3"
-                className="form-control"
-                style={this.props.question && {color: "darkgreen"}}
-                name="summary"
-                rows="5"
-                placeholder={"Instasmart is a question and answer forum for Instacart employees. You can ask about anything Instacart from how to access a quip document to the history of our company values."}
-                onChange={this.props.onChange}
-                defaultValue={this.props.question ? this.props.question.summary : ''}
-                error={this.props.errors.summary}
-              />
+              <h3 id="modal-title">Ask a Question</h3>
+              <div className="container">
+                <div className="mx-1">
+                  <p>Instasmart is a question and answer forum for Instacart employees. You can ask about anything Instacart from how to access a quip document to the history of our company values.</p>
+                </div>
+                <TextArea
+                  label="Question"
+                  labelClass="h5"
+                  optional={false}
+                  className="form-control"
+                  style={this.props.question && {color: "darkgreen"}}
+                  name="summary"
+                  rows="5"
+                  placeholder={"Enter your question here..."}
+                  onChange={this.props.onChange}
+                  defaultValue={this.props.question ? this.props.question.summary : ''}
+                  error={this.props.errors.summary}
+                />
 
-              <TextArea
-                label="Details"
-                labelClass="h5"
-                optional={true}
-                style={this.props.question && {color: "darkgreen"}}
-                className="form-control"
-                name="body"
-                rows="4"
-                placeholder="Add Question Details..."
-                onChange={this.props.onChange}
-                defaultValue={this.props.question ?  this.props.question.body : ''}
-                error={this.props.errors.body}
+                <TextArea
+                  label="Details"
+                  labelClass="h5"
+                  optional={true}
+                  style={this.props.question && {color: "darkgreen"}}
+                  className="form-control"
+                  name="body"
+                  rows="4"
+                  placeholder="Add Question Details..."
+                  onChange={this.props.onChange}
+                  defaultValue={this.props.question ?  this.props.question.body : ''}
+                  error={this.props.errors.body}
 
-              />
+                />
 
-              <TagsInput 
-                label="Tags"
-                labelClass="h5"
-                placeholder="Press tab to add additional tags"
-                name="tag_list"
-                optional={true}
-                tags={this.props.tags}
-                suggestions={this.props.suggestions}
-                handleDeleteTag={this.props.handleDeleteTag}
-                handleAdditionTag={this.props.handleAdditionTag}
-                handleDragTag={this.props.handleDragTag}
-                error={this.props.errors.tags} />
+                <TagsInput 
+                  label="Tags"
+                  labelClass="h5"
+                  placeholder="Press tab to add additional tags"
+                  name="tag_list"
+                  optional={true}
+                  tags={this.props.tags}
+                  suggestions={this.props.suggestions}
+                  handleDeleteTag={this.props.handleDeleteTag}
+                  handleAdditionTag={this.props.handleAdditionTag}
+                  handleDragTag={this.props.handleDragTag}
+                  error={this.props.errors.tags} />
+                </div>
 
             </form>
           </ModalBody>
